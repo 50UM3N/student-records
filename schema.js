9a8535c69e976af3da51604fb1d7a03fdb26be67
Soumen Khara <Soumen2015.s.k@gmail.com>
2020-07-27 23:17:29 +0530

    Adding student schema.js

---
+++ schema.js
@@ -0,0 +1,22 @@
+//creating a scheme for the student record
+const mongoose = require('mongoose');
+const student = new mongoose.Schema({
+    name: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true
+    },
+    phone: {
+        type: String,
+        require: true
+    },
+    degree: [{
+        type: String,
+        require: true
+    }]
+});
+//export that student schema 
+module.exports = new mongoose.model('student', student);
