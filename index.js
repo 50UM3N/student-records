df21b70ae44f37515c16984a190174847c02dcfc
Soumen Khara <Soumen2015.s.k@gmail.com>
2020-07-27 23:20:50 +0530

    Setting views

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 //declare modules and variables
-
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 8080
+const mongoose = require('mongoose');
+const user = require('./schema');
+
+//setting up view engines to ejs
+app.set("view-engine", "ejs");
+
+//use url encoder for get the data 
+app.use(express.urlencoded({
+    extended: false
+}));
 
 //starting the server
 app.listen(port, e => {
