335df6c8d8c3b9bbd7a9c090702e7954bb47cf98
Soumen Khara <Soumen2015.s.k@gmail.com>
2020-07-27 23:49:29 +0530

    Check connection to the database

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 //declare modules and variables
+//checking thar we are not in production
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 8080
 const mongoose = require('mongoose');
 const user = require('./schema');
 
+//connecting to the mongodb database
+mongoose.connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+    })
+    .then(() => {
+        console.log('Database Connected');
+    });
 //setting up view engines to ejs
 app.set("view-engine", "ejs");
 
