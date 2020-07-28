1c589490ca74e6113267c07cc7fcf6085bfa27fb
Soumen Khara <Soumen2015.s.k@gmail.com>
2020-07-28 16:38:29 +0530

    updating the schema for the student image

--- schema.js
+++ schema.js
@@ -16,8 +16,22 @@ const student = new mongoose.Schema({
     degree: {
         type: String,
         require: true
-    }
+    },
+    image: {
+        type: Buffer,
+        required: true
+    },
+    imageType: {
+        type: String,
+        required: true
+    },
 });
+
+student.virtual('studentImage').get(function () {
+    if (this.image != null && this.imageType != null) {
+        return `data:${this.imageType};charset=utf-8;base64,${this.image.toString('base64')}`
+    }
+})
 
 //export that student schema 
 module.exports = new mongoose.model('student', student);
