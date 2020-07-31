97ef961509953e5fe44dacbb93d7be82693447fe
Soumen Khara <Soumen2015.s.k@gmail.com>
2020-07-31 14:59:35 +0530

    Fixing some variables name

--- server.js
+++ server.js
@@ -7,15 +7,13 @@ const flash = require('express-flash')
 const session = require('express-session')
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080
 const mongoose = require('mongoose')
-const student = require('./models/students_scheme');
 const expressEjsLayouts = require('express-ejs-layouts')
 const indexRoute = require('./routes/index')
 const allStudent = require('./routes/students/student')
 const studentCreate = require('./routes/students/create')
 const studentUpdate = require('./routes/students/update')
-const imageTypes = ['image/jpeg', 'image/png', 'images/gif']
 
 //connecting to the mongodb database
 mongoose.connect(process.env.MONGO_URL, {
@@ -27,14 +25,17 @@ mongoose.connect(process.env.MONGO_URL, {
         console.log('Database Connected')
     });
 
+
 //setting up view engines to ejs
 app.set("view-engine", "ejs")
+// set the global layouts
 app.set('layout', 'layouts/layout.ejs')
+// use express ejs layouts
 app.use(expressEjsLayouts)
+// set the static folder
 app.use(express.static('public'))
 //use url encoder for get the data 
 app.use(express.urlencoded({ limit: '10mb', extended: false }));
-
 //use express session for use flash messuage
 app.use(session({ secret: 'soumenkhara', saveUninitialized: true, resave: true }))
 //use flash for the small success and error message display in html
@@ -53,6 +54,6 @@ app.use('/update', studentUpdate)
 // student create route
 app.use('/create', studentCreate)
 //starting the server
-app.listen(port, e => {
-    console.log('Listen on port ', port)
+app.listen(PORT, e => {
+    console.log('Listen on port ', PORT)
 })
