1bb348f44876acff219058744ed6b74d67d9b00a
Soumen Khara <Soumen2015.s.k@gmail.com>
2020-08-14 16:06:40 +0530

    Adding add member panel for admin

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const route = express.Router();
+const user = require("../../models/user_schema");
 const { isAdmin } = require("../routeChecker");
 
 route.use(isAdmin);
 
 route.get("/", (req, res) => {
-  res.render("admin/admin.ejs", { title: "Admin Panel" });
+  user.find({}, (err, data) => {
+    if (err) {
+      req.redirect("/");
+    }
+    if (data) {
+      res.render("admin/admin.ejs", {
+        title: "Admin Panel",
+        user: req.user,
+        users: data,
+      });
+    } else {
+      req.redirect("/");
+    }
+  });
 });
 module.exports = route;
